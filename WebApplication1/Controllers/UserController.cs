db640ebf0503081f671233e1129902575e7e3d1a
Nathan Rice <nrice778@gmail.com>
2021-01-11 15:31:29 -0600

    Added the User Controller and the User View, Added JQuery import script

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            var model = new UserModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(UserModel model)
+        {
+            return View(model);
+        }
+
+        public string Test(string firstName, string lastName)
+        {
+            return $"{firstName} {lastName}";
+        }
+    }
+}
