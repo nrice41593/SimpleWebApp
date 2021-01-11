0595b2e13a2e820adb0f29a1ce63b2da46a9f4d5
Nathan Rice <nrice778@gmail.com>
2021-01-11 15:11:17 -0600

    Finished the UserModel and added a summery for the model

--- WebApplication1/Models/UserModel.cs
+++ WebApplication1/Models/UserModel.cs
@@ -5,7 +5,16 @@ using System.Web;
 
 namespace WebApplication1.Models
 {
+    /// <summary>
+    /// User Information to be submitted through the User creation form in the User/Index view
+    /// </summary>
     public class UserModel
     {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public byte Age { get; set; }
+        public DateTime RegisterDate { get; set; }
+
     }
 }
